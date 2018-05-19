49f6523dc6d82c3ba1b4eedd73c12dc794ad3642
Bhavesh Akoliya <bhaveshakoliya@Bhaveshs-MacBook-Pro.local>
2018-05-19 16:07:36 -0500

    Adding files

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+
+  render() {
+    return (
+
+
+      <div>
+        <div className="Container">
+
+        <button class="search1 search__button"> 1 </button>
+        <button class="search__button"> 2 </button>
+        <button class="search__button"> 3 </button>
+        <button class="search__button"> 4 </button>
+
+        <button className=""> ABC </button>
+          
+        </div>
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
